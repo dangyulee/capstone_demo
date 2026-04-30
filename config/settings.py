ce43ee1d35567a734d3f72cb162c0e53da1d886a
dangyulee <dg200101@gmail.com>
2026-05-01 02:26:38 +0900

    chore

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 LOGIN_URL = "/accounts/login/"
-LOGIN_REDIRECT_URL = "/teams/"
+LOGIN_REDIRECT_URL = "/projects/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
